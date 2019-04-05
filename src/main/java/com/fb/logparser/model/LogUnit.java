d8b9dacd73ca5df00c11a8f6c974bae4b1e0d3e6
Artem Chernousov <adrerek@gmail.com>
2019-04-06 00:10:14 +0300

    Use lombok getters and setter for pretty code

--- src/main/java/com/fb/logparser/model/LogUnit.java
+++ src/main/java/com/fb/logparser/model/LogUnit.java
@@ -1,14 +1,20 @@
 package com.fb.logparser.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Table;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Getter
+@Setter
 public class LogUnit {
 
     public LogUnit(String time, String elapsed, String remotehost, String code_status, String bytes, String method, String url, String username, String peerstatus_peerhost, String type) {
@@ -54,93 +60,5 @@ public class LogUnit {
                 ", peerstatus_peerhost='" + peerstatus_peerhost + '\'' +
                 ", type='" + type + '\'' +
                 '}';
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getElapsed() {
-        return elapsed;
-    }
-
-    public void setElapsed(String elapsed) {
-        this.elapsed = elapsed;
-    }
-
-    public String getRemotehost() {
-        return remotehost;
-    }
-
-    public void setRemotehost(String remotehost) {
-        this.remotehost = remotehost;
-    }
-
-    public String getCode_status() {
-        return code_status;
-    }
-
-    public void setCode_status(String code_status) {
-        this.code_status = code_status;
-    }
-
-    public String getBytes() {
-        return bytes;
-    }
-
-    public void setBytes(String bytes) {
-        this.bytes = bytes;
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPeerstatus_peerhost() {
-        return peerstatus_peerhost;
-    }
-
-    public void setPeerstatus_peerhost(String peerstatus_peerhost) {
-        this.peerstatus_peerhost = peerstatus_peerhost;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 }
